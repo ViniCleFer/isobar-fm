1c2e43d0128cde0b805d4471fcd174902a39a6a3
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2023-09-09 16:44:47 -0300

    feat: implement navigate to details page

--- src/components/ListCardItem/index.tsx
+++ src/components/ListCardItem/index.tsx
@@ -1,6 +1,9 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { BandName, Container, BandAvatar, NumPlays, InfoItems } from './styles';
 
 import { Band } from '../../services/types';
+
 import { getFormattedPlaysOfNumber } from '../../util/getFormattedPlaysOfNumber';
 
 interface ListCardItemProps {
@@ -8,8 +11,14 @@ interface ListCardItemProps {
 }
 
 export function ListCardItem({ band }: ListCardItemProps) {
+  const navigate = useNavigate();
+
+  const handleBand = useCallback(() => {
+    navigate(`details/${band?.id}`);
+  }, [navigate, band?.id]);
+
   return (
-    <Container>
+    <Container onClick={handleBand}>
       <BandAvatar src={band.image} alt='image-band' />
 
       <InfoItems>
