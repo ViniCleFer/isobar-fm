4b228cf91fee1122a9d8a39eff34ccb09cd50816
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2023-09-09 11:10:11 -0300

    feat: add root styles and remove css files

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material';
+
+export default createTheme({
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+  palette: {
+    primary: {
+      main: '#F05D31',
+    },
+    grey: {
+      '300': '#D9D9D9',
+      '500': '#656668',
+    },
+  },
+});
