150691803f3495a6775143d1176dac2059de911b
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2023-09-10 19:59:36 -0300

    feat: adding band details page

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,21 +1,59 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AddOutlined, ArrowBackIosRounded } from '@mui/icons-material';
 
 import { api } from '../../services/api';
-import { Band } from '../../services/types';
+import { Album, Band } from '../../services/types';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  GoBackButton,
+  BandPhotoContainer,
+  BandInfoContainer,
+  BandName,
+  BandInfoBox,
+  BandInfoText,
+  BandAvatar,
+  Biography,
+  DividerContainer,
+  AlbumsTitle,
+  GridContainer,
+} from './styles';
+
+import { getFormattedPlaysOfNumber } from '../../util/getFormattedPlaysOfNumber';
+
+import { Logo } from '../../components/Logo';
 
 export function Details() {
   const params = useParams();
+  const navigate = useNavigate();
 
   const [selectedBand, setSelectedBand] = useState<Band>({} as Band);
+  const [recentlyReleasedAlbum, setRecentlyReleasedAlbum] = useState<Album>(
+    {} as Album
+  );
 
   const loadBandById = useCallback(async () => {
     const response = await api(`bands/${params?.id}`);
 
     if (response !== null) {
       setSelectedBand(response);
+
+      if (response?.albums?.length > 0) {
+        const albums = response.albums.map(async (albumId: string) => {
+          const albumResponse = await api(`albums/${albumId}`);
+
+          return albumResponse;
+        });
+
+        const albumsResponse = await Promise.all(albums);
+
+        setSelectedBand((prevState) => ({
+          ...prevState,
+          foundedAlbums: albumsResponse,
+        }));
+      }
     }
   }, [params?.id]);
 
@@ -25,9 +63,79 @@ export function Details() {
     }
   }, [params?.id]);
 
+  useEffect(() => {
+    if (
+      selectedBand?.foundedAlbums &&
+      selectedBand?.foundedAlbums?.length > 0
+    ) {
+      const recentAlbum = selectedBand?.foundedAlbums.reduce(
+        (prevAlbum, currentAlbum) => {
+          if (prevAlbum.releasedDate < currentAlbum.releasedDate) {
+            return currentAlbum;
+          } else {
+            return prevAlbum;
+          }
+        }
+      );
+
+      setRecentlyReleasedAlbum(recentAlbum);
+    }
+  }, [selectedBand?.foundedAlbums]);
+
+  const handleGoBack = useCallback(() => {
+    navigate(-1);
+  }, [navigate]);
+
   return (
     <Container>
-      <pre>{JSON.stringify(selectedBand, null, 2)}</pre>
+      <Header>
+        <GoBackButton onClick={handleGoBack}>
+          <ArrowBackIosRounded />
+        </GoBackButton>
+
+        <Logo />
+      </Header>
+      <BandPhotoContainer>
+        <img
+          src={selectedBand?.image}
+          alt={selectedBand?.name}
+          width='100%'
+          height='100%'
+        />
+      </BandPhotoContainer>
+      <BandInfoContainer>
+        <BandInfoText>{recentlyReleasedAlbum?.name}</BandInfoText>
+        <BandInfoBox>
+          <BandName>{selectedBand?.name}</BandName>
+          <BandAvatar
+            src={selectedBand?.image}
+            alt={selectedBand?.name}
+            width={100}
+            height={100}
+          />
+        </BandInfoBox>
+        <BandInfoText>
+          {getFormattedPlaysOfNumber(selectedBand?.numPlays)}
+        </BandInfoText>
+      </BandInfoContainer>
+
+      <Biography
+        dangerouslySetInnerHTML={{ __html: selectedBand?.biography }}
+      />
+
+      <DividerContainer>
+        <AddOutlined />
+      </DividerContainer>
+
+      <AlbumsTitle>Albuns</AlbumsTitle>
+
+      {selectedBand?.foundedAlbums && (
+        <GridContainer>
+          {selectedBand?.foundedAlbums?.map((item) => (
+            <img src={item.image} alt={item.name} width='100%' loading='lazy' />
+          ))}
+        </GridContainer>
+      )}
     </Container>
   );
 }
--- src/pages/Details/styles.ts
+++ src/pages/Details/styles.ts
@@ -1,14 +1,107 @@
 import { styled } from '@mui/material/styles';
-import { Container as MuiContainer } from '@mui/material';
+import {
+  Box,
+  Divider,
+  IconButton,
+  Container as MuiContainer,
+  Typography,
+} from '@mui/material';
 
 export const Container = styled(MuiContainer)`
   padding: 0 !important;
-`;
-
-export const ListContainer = styled('main')`
-  background-color: ${({ theme }) => theme.palette.common.white};
   display: flex;
   flex-direction: column;
   height: 100%;
+  width: 100%;
+  margin-bottom: 24px;
+`;
+
+export const Header = styled(Box)`
+  background-color: ${({ theme }) => theme.palette.primary.main};
   padding: 18px;
+  width: 100%;
+  display: flex;
+  height: 92px;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const GoBackButton = styled(IconButton)`
+  position: absolute;
+  left: 18px;
+
+  svg {
+    color: ${({ theme }) => theme.palette.common.white};
+  }
+`;
+
+export const BandPhotoContainer = styled(Box)`
+  height: 370px;
+`;
+
+export const BandInfoContainer = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 18px;
+`;
+
+export const BandInfoBox = styled(Box)`
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: -50px;
+  position: relative;
+`;
+
+export const BandName = styled(Typography)`
+  position: absolute;
+  top: -40px;
+  width: 100%;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  white-space: nowrap;
+  color: ${({ theme }) => theme.palette.common.white};
+  font-size: 24px;
+  font-weight: 500;
+`;
+
+export const BandInfoText = styled(Typography)`
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.palette.grey[500]};
+  padding-top: 12px;
+  font-size: 14px;
+  font-weight: 400;
+  max-width: 30%;
+  text-align: center;
+  white-space: wrap;
+`;
+
+export const BandAvatar = styled('img')`
+  border-radius: 50%;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.15);
+`;
+
+export const Biography = styled(Typography)`
+  padding: 18px;
+  color: ${({ theme }) => theme.palette.grey[900]};
+`;
+
+export const DividerContainer = styled(Divider)`
+  padding: 18px;
+`;
+
+export const AlbumsTitle = styled(Typography)`
+  display: flex;
+  justify-content: center;
+  text-transform: uppercase;
+  margin-bottom: 16px;
+`;
+
+export const GridContainer = styled('div')`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0;
 `;
